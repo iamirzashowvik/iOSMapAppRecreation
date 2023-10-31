3079a3df10e5796c6fbd34e0f0387e22caa69e37
Mirza Showvik <showvikmirza@gmail.com>
2023-10-31 10:03:19 +0600

    Initial Commit

---
+++ MapsRecreation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapsRecreation
+//
+//  Created by Mirza Showvik on 31/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapsRecreation/MapsRecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsRecreationApp.swift
+//  MapsRecreation
+//
+//  Created by Mirza Showvik on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MapsRecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
